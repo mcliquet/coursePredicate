0fa406c5b511dc07fb5cba5da8cd6fbdc01ab36e
Marcus <cliquet@gmail.com>
2019-07-01 11:13:58 -0300

    Method reference static

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Product;
-import util.ProductPredicate;
+//import util.ProductPredicate;
 
 public class Program {
 
@@ -17,7 +17,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(new ProductPredicate());
+		//implementação do predicado
+		//list.removeIf(new ProductPredicate());
+		
+		//referência de método estático
+		list.removeIf(Product::staticProductPredicate);
 		
 		for (Product p : list) {
 			System.out.println(p);
